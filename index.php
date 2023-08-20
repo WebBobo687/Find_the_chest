9fe4967877dd98aa12d13f619d5119937dc43dc7
odahNC <tcorentin1345@gmail.com>
2023-08-21 10:33:13 +1100

    ✨ Importation du fichier de la page d'accueil

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue | FindTheChest</title>
+</head>
+<body>
+    
+</body>
+</html>
