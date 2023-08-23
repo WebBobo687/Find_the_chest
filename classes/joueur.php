90dd85cae2ac246c54d4d35e28d5a3eb14783601
RoiattiBrandon <roiatti.brandon@users.noreply.github.com>
2023-08-23 14:44:24 +1100

    refactor classes Joueur && Monstre

--- classes/joueur.php
+++ classes/joueur.php
@@ -4,61 +4,23 @@ declare(strict_types=1);
 namespace Classes;
 use Classes\Perso;
 
-$win = False;
-
 class Joueur extends Perso {
 
     public int $exp;
-    public bool $win;
+
+    public function __construct($pv, $atq, $posX, $posY, $exp) {
+        parent::__construct($pv, $atq, $posX, $posY);
+        $this->exp = $exp;
+    }
 
     // __setter__
-    public function setPv($pv)
-    {
-        parent::setPv($pv);
-    }
-
-    public function setAtq($atq)
-    {
-        parent::setAtq($atq);
-    }
-
-    public function setPosX($posX)
-    {
-        parent::setPosX($posX);
-    }
-    
-    public function setPosY($posY)
-    {
-        parent::setPosY($posY);
-    }
-
     protected function setExp($exp)
     {
         $this->$exp = 0;
     }
 
     // __getter__
-    public function getPv()
-    {
-        return $this->pv;
-    }
-
-    public function getAtq()
-    {
-        return $this->atq;
-    }
-
-    public function getPosX()
-    {
-        return $this->posX;
-    }
-    
-    public function getPosY()
-    {
-        return $this->posY;
-    }
-
-    public function getExp()
+        public function getExp()
     {
         return $this->exp;
     }
--- classes/monstre.php
+++ classes/monstre.php
@@ -6,45 +6,7 @@ use Classes\Perso;
 
 class Monstre extends Perso
 {
-    // __setter__
-    public function setPv($pv)
-    {
-        parent::setPv($pv);
-    }
-
-    public function setAtq($atq)
-    {
-        parent::setAtq($atq);
-    }
-
-    public function setPosX($posX)
-    {
-        parent::setPosX($posX);
-    }
-    
-    public function setPosY($posY)
-    {
-        parent::setPosY($posY);
-    }
-
-    // __getter__
-    public function getPv()
-    {
-        return $this->pv;
-    }
-
-    public function getAtq()
-    {
-        return $this->atq;
-    }
-
-    public function getPosX()
-    {
-        return $this->posX;
-    }
-
-    public function getPosY()
-    {
-        return $this->posY;
+    public function __construct($pv, $atq, $posX, $posY) {
+        parent::__construct($pv, $atq, $posX, $posY);
     }
 }
